b01a931c2d5b789f93cc4f8de2bb743fe7d73959
Lilibeth Mejia <mejialily036@gmail.com>
2023-07-24 16:19:38 -0500

    added readFile and writeFile

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
 const db = require("./db/db.json");
+const fs = require('fs');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -12,6 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
@@ -19,12 +24,29 @@ app.get('/api/notes', (req, res) => res.json(db));
 
 app.post('/api/notes', (req, res) => {
     console.info(`${req.method} request received`);
+
     const { title, text } = req.body;
+
     if (title && text) {
         const newNote = {
             title,
             text
         };
+
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) {
+        console.log(err);
+        return
+        } else {
+            const parsedData = JSON.parse(data)
+            parsedData.push(newNote);
+
+            fs.writeFile(`./db/db.json`, JSON.stringify(parsedData, null, 2), (writeErr) =>
+                writeErr ? console.error(err) : console.log(`New note for ${newNote.title} has been written to JSON file`)
+                );
+            }
+        });
+
         const response = {
             status: 'succes',
             body: newNote,
